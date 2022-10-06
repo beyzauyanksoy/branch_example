ef159d158b546d628eefa62fddac76182fdfe0c4
Beyza <beyzauyanik57@hotmail.com>
2022-10-06 11:36:47 +0300

    text change

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,9 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
+        
         child: Column(
+        
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -96,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'beyza uyanıksoy',
             ),
             Text(
               '$_counter',
